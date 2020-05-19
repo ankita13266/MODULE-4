d1fb7b207517898fb201cc4a256af3cd65fcdd59
ankita13266 <65207724+ankita13266@users.noreply.github.com>
2020-05-19 16:49:16 +0530

    delete

--- SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var byeSpeaker = new object();
-    var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name){
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
