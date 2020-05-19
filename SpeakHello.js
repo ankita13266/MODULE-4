b76b277a0fa8482736c5482bf4b89ffcca87075e
ankita13266 <65207724+ankita13266@users.noreply.github.com>
2020-05-19 16:54:44 +0530

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-    var helloSpeaker = new object();
-    var speakWord = "Hello";
-    helloSpeaker.speak = function speak(name){
-        console.log(speakWord + " " + name);
-    };
-    window.helloSpeaker = helloSpeaker;
-})(window);
