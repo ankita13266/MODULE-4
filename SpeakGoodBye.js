906e5c7e64130b470223d64ea6d15dbb9ea63839
ankita13266 <65207724+ankita13266@users.noreply.github.com>
2020-05-19 17:01:33 +0530

    ADD

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+function(window){
+    var byeSpeaker = new object();
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function (name){
+        console.log(speakWord + " " + name);
+    };
+    window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window){
+    var helloSpeaker = new object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name){
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
+})(window);
